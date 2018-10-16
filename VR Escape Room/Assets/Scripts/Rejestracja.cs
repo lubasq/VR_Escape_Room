402821ec9796e8c0eb6644039710cc5c81bfc73a
maydanv9 <dawid.fifa@interia.pl>
2018-10-16 20:57:02 +0200

    projekt

---
+++ VR Escape Room/Assets/Scripts/Rejestracja.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mono.Data.Sqlite;
+using System.Data;
+using System;
+using TMPro;
+
+public class Rejestracja : MonoBehaviour
+{
+    public TMP_InputField login;
+    public TMP_InputField password;
+    public TMP_InputField email;
+
+
+    private void Start()
+    {
+
+    }
+    // Use this for initialization
+    public void RegisterPlayer()
+    {
+        string conn = "URI=file:" + Application.dataPath + "/Database.db"; //Path to database.
+        IDbConnection dbconn;
+        dbconn = (IDbConnection)new SqliteConnection(conn);
+        dbconn.Open(); //Open connection to the database.
+        IDbCommand dbcmd = dbconn.CreateCommand();
+        Debug.Log(email.text);
+        Debug.Log(login.text);
+        Debug.Log(password.text);
+        string sqlQuery = "INSERT INTO Users (login, password, email) VALUES ('" + login.text + "',' " + password.text + "' ,'" + email.text + "');";
+        dbcmd.CommandText = sqlQuery;
+        IDataReader reader = dbcmd.ExecuteReader();
+
+        reader.Close();
+        reader = null;
+        dbcmd.Dispose();
+        dbcmd = null;
+        dbconn.Close();
+        dbconn = null;
+    }
+}
