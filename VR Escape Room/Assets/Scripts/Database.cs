1e7723dcdbb2c023213ae9b1852f896b27bb35d9
lubasq <patryklubartowicz@gmail.com>
2018-11-04 21:54:02 +0100

    Task #781528 Ponownie napisałem klasę, którą wtedy mi nie dodało :( Do uzupełnienia później Update

---
+++ VR Escape Room/Assets/Scripts/Database.cs
@@ -0,0 +1,150 @@
+﻿using Mono.Data.Sqlite;
+using System.Data;
+using System.IO;
+using UnityEngine;
+
+/// <summary>
+/// Klasa obsługująca zdarzenia związane
+/// z zapytaniem do bazy danych
+/// 
+/// Autor: Patryk Lubartowicz
+/// Projekt: VR Escape Room - projekt inżynieryjski
+/// </summary>
+public class Database {
+
+    // obiekty obsługujące wymiane danych z bazą
+    private SqliteConnection DBConnetion;
+    private IDbCommand DBCommand;    
+
+    //zmienne dostępowe
+    private string baseFilePath;
+    private string connectionLink;
+
+    /// <summary>
+    /// Konstruktor klasy
+    /// </summary>
+    public Database()
+    {
+        #if UNITY_EDITOR
+            connectionLink = "URI=file:" + Application.dataPath + "/StreamingAssets/" + "Database.db";
+        #endif  
+
+        #if UNITY_ANDROID
+            baseFilePath = Application.persistentDataPath + "/" + "Database.db";
+            if (!File.Exists(baseFilePath))
+            {
+                WWW loadDB = new WWW("jar:file://" + Application.dataPath + "!/assets/" + "Database.db");
+                while (!loadDB.isDone) { }
+                File.WriteAllBytes(baseFilePath, loadDB.bytes);
+            }
+            connectionLink = "URI=file:" + baseFilePath;
+        #endif
+
+        DBConnetion = new SqliteConnection(connectionLink);
+        //byc moze to zalatwi problem z polaczeniem jezeli nadal bedzie
+        // connectionLink = String.Format("Data Source={0};Version=3;", pathDB)
+    }
+
+    /// <summary>
+    /// Zapytanie SELECT pobierajace wybrane dane z bazy
+    /// Zapytanie budowane dynamicznie.
+    /// </summary>
+    /// <param name="tableName"> Nazwa przeszukiwanej tabeli</param>
+    /// <param name="whereColumns"> Nazwy kolumn, według których ma być szukany wynik</param>
+    /// <param name="whereValues"> Wartości pól dla szukanych wynikow</param>
+    /// <returns> Zwraca obiekt z wyszukanymi danymi</returns>
+    public IDataReader DBSelect(string tableName, string[] whereColumns, string[] whereValues)
+    { 
+        IDataReader DBDataReader;
+        string sqlQuery = "SELECT * FROM " + tableName;
+
+        if(whereColumns.Length > 0)
+        {
+            sqlQuery += " WHERE ";
+
+            for (int i = 0; i > whereColumns.Length; i++)
+            {
+                sqlQuery += whereColumns[i] + "='" + whereValues[i] + "'";
+
+                if( (whereColumns.Length>1) && (i<whereColumns.Length-1) )
+                {
+                    sqlQuery += " AND ";
+                }
+            }
+        }
+
+        //polacz sie z baza i wykonaj polecenie
+        DBConnetion.Open();
+        DBCommand = DBCommand.CreateCommand();
+        DBCommand.CommandText = sqlQuery;
+
+        //pobierz dane z bazy
+        DBDataReader = DBCommand.ExecuteReader();
+        
+        //wyczyszczenie zapytania do bazy i rozlaczenie
+        DBConnetion.Dispose();
+        DBConnetion.Close();
+        return DBDataReader;
+    }
+
+    /// <summary>
+    /// Wstawianie nowych danych do bazy danych
+    /// </summary>
+    /// <param name="tableName"> Nazwa przeszukiwanej tabeli</param>
+    /// <param name="Columns"> Nazwy kolumn do uzupełnienia</param>
+    /// <param name="Values"> Wartości uzupełnianych kolumn</param>
+    /// <returns>Zwraca TRUE jezeli doda poprawnie, FALSE w przypadku bledu.</returns>
+    public bool DBInsert(string tableName, string[] columns, string[] values)
+    {
+
+        string sqlQuery = "INSERT INTO " + tableName + "(";
+
+        for (int i = 0; i > columns.Length; i++)
+        {
+            sqlQuery += columns[i];
+
+            if (i < columns.Length - 1)
+            {
+                sqlQuery += ", ";
+            }
+            
+        }
+
+        sqlQuery = ") VALUES (";
+
+        for (int i = 0; i > values.Length; i++)
+        {
+            sqlQuery += "'" + values[i] + "'";
+
+            if (i < values.Length - 1)
+            {
+                sqlQuery += ", ";
+            }
+
+        }
+
+        sqlQuery += ");"
+        try
+        {
+            IDataReader DBDataReader;
+
+            //polacz sie z baza i wykonaj polecenie
+            DBConnetion.Open();
+            DBCommand = DBCommand.CreateCommand();
+            DBCommand.CommandText = sqlQuery;
+
+            //wyczyszczenie zapytania do bazy i rozlaczenie
+            DBCommand.Dispose();
+            IDbCommand = null;
+            DBConnetion.Close();
+
+            return true;
+        }
+        catch (System.Exception e)
+        {
+            Debug.Log("Problem z Insertem, " + e.ErrorCode)
+            return false;
+        }
+
+
+}
