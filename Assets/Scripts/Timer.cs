384a0d64ccf84779cc233fb9512bd2bc62fb4c78
maydanv9 <dawid.fifa@interia.pl>
2018-11-13 17:33:30 +0100

    Dodanie timera

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class Timer : MonoBehaviour {
+    public TMP_Text timer;
+    private float startTime;
+
+    void Start () {
+        startTime = Time.time;
+    }
+	
+	void Update () {
+        float t = Time.time - startTime;
+
+        string minutes = ((int)t / 60).ToString();
+        string seconds = (t % 60).ToString("f0");
+        if (t >= 60)
+        {
+            timer.text = minutes + "m " + seconds + "s";
+        }
+        else
+        {
+            timer.text = seconds + "s";
+        }
+    }
+}
