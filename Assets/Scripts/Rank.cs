2492a118c9ae760ccaf99878a0dc91c532a33d4e
lubasq <patryklubartowicz@gmail.com>
2018-11-19 17:20:06 +0100

    #781528 Skrypty bazodanowe dla aplikacji Dodano ranking zalogowanego gracza.

--- Assets/Scripts/Rank.cs
+++ Assets/Scripts/Rank.cs
@@ -7,6 +7,9 @@ using UnityEngine;
 public class Rank : MonoBehaviour {
     public TMP_Text playersLogin;
     public TMP_Text playersTime;
+    public TMP_Text loggedPlayerPosition;
+    public TMP_Text loggedPlayerTime;
+
     // Use this for initialization
     void Start () {
         RankOfBestPlayers();
@@ -30,15 +33,35 @@ public class Rank : MonoBehaviour {
             times += reader.GetString(1) + "\n";
         }
 
+        if (!logins.Contains("user3"))
+        {
+            //Debug.Log("dziala");
+            RankOfLoggedPlayer("user3");
+        }
+
         playersLogin.text = logins;
         playersTime.text = times;
     }
 
     //@TODO
-    private void RankOfLoggedPlayer()
+    private void RankOfLoggedPlayer(string username)
     {
         global::Database user = new global::Database();
-        //IDataReader reader = user.DBSelect("Scores", new string[] { "Us.login", "Sc.game_time" }, new string[] { }, new string[] { }, new string[] { "Users", "Levels" }, "Sc.game_time ASC LIMIT 10");
+        IDataReader reader = user.DBSelect("Scores", new string[] { "(SELECT count(*) FROM Scores Sc1 WHERE Sc.game_time >= Sc1.game_time) AS Position", "Sc.game_time" }, new string[] {"Us.login"}, new string[] {username}, new string[] { "Users", "Levels" }, "Sc.game_time ASC LIMIT 1");
+        int position = 0;
+        string time = "";
+
+        while (reader.Read())
+        {
+            position = reader.GetInt32(0);
+            time += reader.GetString(1);
+        }
+
+        if(position > 0)
+        {
+            loggedPlayerPosition.text = position.ToString();
+            loggedPlayerTime.text = time;
+        }
         
     }
 }
