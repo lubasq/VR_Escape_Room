9fcd4527e2ecdbf84a2c5940ba1f515c7a3f4625
maydanv9 <dawid.fifa@interia.pl>
2018-12-02 18:07:26 +0100

    Pointer, teleporter i skrypty

---
+++ Assets/Mover.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+    public GameObject Arrow;
+    public float yAxis = 0.14f;
+
+    void Update()
+    {
+
+        Arrow.transform.Translate(Vector3.up * yAxis);
+    }
+}
