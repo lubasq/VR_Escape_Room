2f618b63a459db32ac88d3eb06f8c4c07ee1d6c5
Merafil <merafilkaven@gmail.com>
2018-12-16 17:42:44 +0100

    Added Interactions

---
+++ Assets/HitReceiver.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitReceiver : MonoBehaviour
+{
+
+    public Camera viewCamera;
+    private Animator anim;
+
+    // Use this for initialization
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    public void OnRayHit()
+    {
+                if (Input.GetButtonDown("Fire1") && !anim.GetBool("isOpen")) {
+                    anim.SetBool("isOpen", true);
+                }
+                else if (Input.GetButtonDown("Fire1") && anim.GetBool("isOpen")) {
+                    anim.SetBool("isOpen", false);
+
+                }
+            }
+        }
