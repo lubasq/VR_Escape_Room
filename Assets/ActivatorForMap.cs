418a211324159d831fd4939770d35212f586e2bc
Merafil <merafilkaven@gmail.com>
2018-12-29 14:56:40 +0100

    new books animation Added Map for third riddle

---
+++ Assets/ActivatorForMap.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivatorForMap : MonoBehaviour {
+
+    private HitReceiver hitScript;
+
+    // Use this for initialization
+    void Start () {
+        hitScript = GetComponent<HitReceiver>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(GameVariables.gotPin == true) {
+            hitScript.enabled = true;
+        }
+	}
+}
--- Assets/Scripts/GameVariables.cs
+++ Assets/Scripts/GameVariables.cs
@@ -6,4 +6,5 @@ public class GameVariables : MonoBehaviour {
 
     public static int keyCount;
     public static bool buttonPressed = false;
+    public static bool gotPin = false;
 }
